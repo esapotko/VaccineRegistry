3a14b8170f9d8e0f1776ad3a3b44ab050f388299
Esa Potkonen <esa.potkonen@gmail.com>
2021-03-27 16:57:19 +0200

    Initial commit

---
+++ vaccine-registry/src/main/java/com/potkonen/vaccine/registry/VaccineRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.potkonen.vaccine.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccineRegistryApplication.class, args);
+	}
+
+}
---
+++ vaccine-registry/src/test/java/com/potkonen/vaccine/registry/VaccineRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potkonen.vaccine.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
